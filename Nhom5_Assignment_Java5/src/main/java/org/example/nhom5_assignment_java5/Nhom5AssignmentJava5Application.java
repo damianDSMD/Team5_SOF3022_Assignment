ffb248b39cfe6d94f72a9ad5e18bd56e6a8a8cdf
Desmond Da Migu <tintin70189@gmail.com>
2025-10-02 22:14:22 +0700

    Create Assignment Project
    This uses Gradle Groovy, JDK 17
    
    The code to connect to your database is located in build.gradle

---
+++ Nhom5_Assignment_Java5/src/main/java/org/example/nhom5_assignment_java5/Nhom5AssignmentJava5Application.java
@@ -0,0 +1,13 @@
+package org.example.nhom5_assignment_java5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhom5AssignmentJava5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nhom5AssignmentJava5Application.class, args);
+    }
+
+}
---
+++ Nhom5_Assignment_Java5/src/test/java/org/example/nhom5_assignment_java5/Nhom5AssignmentJava5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nhom5_assignment_java5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom5AssignmentJava5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
