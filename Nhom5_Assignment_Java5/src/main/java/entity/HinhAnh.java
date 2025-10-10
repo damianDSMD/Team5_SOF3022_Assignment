6caca368653fff4cb6e1e7d09314e20cd34cb984
Desmond Da Migu <tintin70189@gmail.com>
2025-10-10 19:13:33 +0700

    c

---
+++ Nhom5_Assignment_Java5/src/main/java/entity/HinhAnh.java
@@ -0,0 +1,30 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "HinhAnh")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HinhAnh {
+
+    @Id
+    @Column(name = "MaHA", length = 50)
+    private String maHA;
+
+    @ManyToOne
+    @JoinColumn(name = "MaSP")
+    @JsonIgnore
+    private SanPham sanPham;
+
+    @Column(name = "TenHA")
+    private String tenHA;
+
+    @Column(name = "FilePath", length = 500)
+    private String filePath;
+}
